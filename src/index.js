783fb573f6676bfd5acdf805d478eb428c33c500
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-04-12 16:07:34 +0200

    router works

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 //console.log('hi');
 import React from 'react';
-import { Router, browerHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import {render} from 'react-dom';
 import 'babel-polyfill';
 import routes from './routes';
-import '.styles/styles.css';
-import '../node_modules/bootstrap/dist/css/bootstrap-theme.min';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 render(
-    <Router history={browerHistory} routes={routes}/>,
-    document.createElementById('app')
+    <Router history={browserHistory} routes={routes}/>,
+    document.getElementById('app')
 );

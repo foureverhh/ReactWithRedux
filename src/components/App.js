2fe533e8aeddfd5ddf0d5ba4efcd4d308270cd03
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-04-12 16:27:20 +0200

    Set a clickable header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, {PropTypes} from 'react';
+import Header from '../common/Header';
 
 class App extends React.Component {
     render(){
         return (
             <div className="container-filuid">
-                <p>Header here...</p>
+                <Header />
                 {this.props.children || "Welcome to Home page"}
             </div>
         );
